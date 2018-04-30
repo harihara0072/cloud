ba410c2deac46ec248b1663d6a44bb852b041922
hari nakshatrala <hari.kummi@gmail.com>
2018-04-30 02:36:07 -0500

    bdhsb

--- photo.py
+++ photo.py
@@ -1,22 +1,22 @@
-from flask import Flask
 from google.cloud import storage
 import os
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-
     # Imports the Google Cloud client library
-    #from google.cloud import storage
+    # from google.cloud import storage
     # Instantiates a client
     client = storage.Client()
     for bucket in client.list_buckets():
         print(bucket)
+
     bucket_name = "haricloudbucket"
-	storage_client = storage.Client()
+    storage_client = storage.Client()
+
+
     bucket = storage_client.get_bucket(bucket_name)
 
     blobs = bucket.list_blobs()
@@ -24,7 +24,6 @@ def hello_world():
     for blob in blobs:
         print(blob.name)
     return "Hello hari"
-
 
 if __name__ == '__main__':
     app.run()
